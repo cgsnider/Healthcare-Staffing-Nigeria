6b516b0f4810eb7072bbe3b766977bb61a383bbf
WilliamLeonard <wleonard3@gatech.edu>
2021-11-17 12:55:22 -0500

    Job Listing and Grid Components 2

--- health_staffing_nigeria/src/App.js
+++ health_staffing_nigeria/src/App.js
@@ -3,6 +3,30 @@ import './App.css';
 import Login from './components/Login';
 import Listing from './components/Listing';
 import JobListGrid from './components/JobListGrid';
+import Register from "./components/Register.js";
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
 
 function App() {
   const mockJob = <Listing link="http://localhost:3000" image='Resources/mediumnew.png' position="ER Physician" location="Generic, Nigeria" shifts="12 Hour Shifts" salary="$70,000"/>;
