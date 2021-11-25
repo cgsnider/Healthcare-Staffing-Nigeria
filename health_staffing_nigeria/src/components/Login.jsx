55970ee74823dc9d3deeb8ec87d2286195ef1d2f
Dinn3r <91625524+Dinn3r@users.noreply.github.com>
2021-11-24 22:37:29 -0500

    Login Component

--- health_staffing_nigeria/src/components/Login.jsx
+++ health_staffing_nigeria/src/components/Login.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Form from "react-bootstrap/Form";
+import Button from 'react-bootstrap/Button';
+import './LoginStyles.css';
 // import { StyleSheet, Text, View } from 'React'
 
 export default function Login() {
+
+	const [username, getName] = useState("");
+	const [password, getPass] = useState("");
+
+	function handleLogin() {
+		event.preventDefault();
+	}
+
+	function checkInput() {
+		return username.length > 0 && password.length > 0;
+	}
+
     return (
-        <h1> Test </h1>
-    )
+        <div className = "Login">
+        	<Form onSubmit = {handleLogin}>
+        		<Form.Group controlID = "username" size = "lg">
+        			<Form.Label>Username</Form.Label>
+        			<Form.Control autofocus 
+        						  type = "username" 
+        						  value = {username} 
+        						  onChange = {(n) => getName(n.target.value)}/>
+        		</Form.Group>
+
+        		<Form.Group controlID = "password" size = "lg">
+        			<Form.Label>Password</Form.Label>
+        			<Form.Control type = "password"
+        						  value = {password}
+        						  onChange = {(n) => getPass(n.target.value)}/>
+        		</Form.Group>
+
+        		<Button className = "loginButton" variant = "primary" size = "lg" type = "submit" disabled = {!checkInput()}>
+        			Login
+        		</Button>
+        	</Form>
+        </div>
+    );
 }
 
