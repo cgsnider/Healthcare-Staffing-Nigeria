5c2188b365fc4e42187a8ca19c34d2f69e0e3cb4
Dinn3r <91625524+Dinn3r@users.noreply.github.com>
2021-11-28 13:06:43 -0500

    Add files via upload

--- health_staffing_nigeria/src/components/Login.jsx
+++ health_staffing_nigeria/src/components/Login.jsx
@@ -1,9 +1,104 @@
-import React from 'react'
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./LoginStyles.css";
 // import { StyleSheet, Text, View } from 'React'
 
 export default function Login() {
-    return (
-        <h1> Test </h1>
-    )
+  const [username, getName] = useState("");
+  const [password, getPass] = useState("");
+
+  var testUsername = "test";
+  var testPassword = "password";
+  var tryAgain = false;
+
+  function checkInput() {
+    return username.length > 0 && password.length > 0;
+  }
+
+  //link register page
+  function toRegister() {
+    return null;
+  }
+
+  //Check username and password
+  function checkLogin() {
+    if (username == testUsername && password == testPassword) {
+      console.log("success");
+      tryAgain = false;
+      console.log(tryAgain);
+    } else {
+      tryAgain = true;
+      incorrect();
+    }
+  }
+
+  function incorrect() {
+    if (tryAgain == true) {
+      console.log("test");
+      return <div className="incorrect">Incorrect username or password!</div>;
+    }
+  }
+
+  return (
+    <div className="Login">
+      <div className="Title">Login</div>
+      {incorrect()}
+
+      <Form onSubmit={checkLogin}>
+        <Form.Group size="lg" controlid="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            autoFocus
+            type="username"
+            value={username}
+            onChange={(n) => getName(n.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlid="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(n) => getPass(n.target.value)}
+          />
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          size="lg"
+          type="submit"
+          disabled={!checkInput()}
+        >
+          Login
+        </Button>
+        <div className="registerTag">Need an account?</div>
+        <div className="registerButton">
+          <Button
+            variant="primary"
+            size="lg"
+            type="register"
+            onClick={() => {
+              toRegister();
+            }}
+          >
+            Register
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+}
+
+export class LoginBuilder {
+	
+	constructor() {
+		//Record data for props (looks like there are no props here)
+	}
+
+	toJSX() {
+		return <Login />
+	}
+
 }
 
