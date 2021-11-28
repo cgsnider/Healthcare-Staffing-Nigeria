2d6dd780aad55f031bd6ca5cb089b6073d6aa6c2
Dinn3r <91625524+Dinn3r@users.noreply.github.com>
2021-11-27 23:36:26 -0500

    Login

--- health_staffing_nigeria/src/components/Login.jsx
+++ health_staffing_nigeria/src/components/Login.jsx
@@ -1,46 +1,93 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
-import './LoginStyles.css';
+import Button from "react-bootstrap/Button";
+import "./LoginStyles.css";
 // import { StyleSheet, Text, View } from 'React'
 
-export default function Login(props) {
+export default function Login() {
+  const [username, getName] = useState("");
+  const [password, getPass] = useState("");
 
-	const [username, getName] = useState("");
-	const [password, getPass] = useState("");
+  var testUsername = "test";
+  var testPassword = "password";
+  var tryAgain = false;
 
-	function handleLogin() {
-		// event.preventDefault();
-	}
+  function checkInput() {
+    return username.length > 0 && password.length > 0;
+  }
 
-	function checkInput() {
-		return username.length > 0 && password.length > 0;
-	}
+  //link register page
+  function toRegister() {
+    return null;
+  }
 
-    return (
-        <div className = "Login">
-        	<Form onSubmit = {handleLogin}>
-        		<Form.Group controlID = "username" size = "lg">
-        			<Form.Label>Username</Form.Label>
-        			<Form.Control autofocus 
-        						  type = "username" 
-        						  value = {username} 
-        						  onChange = {(n) => getName(n.target.value)}/>
-        		</Form.Group>
+  //Check username and password
+  function checkLogin() {
+    if (username == testUsername && password == testPassword) {
+      console.log("success");
+      tryAgain = false;
+      console.log(tryAgain);
+    } else {
+      tryAgain = true;
+      incorrect();
+    }
+  }
 
-        		<Form.Group controlID = "password" size = "lg">
-        			<Form.Label>Password</Form.Label>
-        			<Form.Control type = "password"
-        						  value = {password}
-        						  onChange = {(n) => getPass(n.target.value)}/>
-        		</Form.Group>
+  function incorrect() {
+    if (tryAgain == true) {
+      console.log("test");
+      return <div className="incorrect">Incorrect username or password!</div>;
+    }
+  }
 
-        		<Button className = "loginButton" variant = "primary" size = "lg" type = "submit" disabled = {!checkInput()}>
-        			Login
-        		</Button>
-        	</Form>
+  return (
+    <div className="Login">
+      <div className="Title">Login</div>
+      {incorrect()}
+
+      <Form onSubmit={checkLogin}>
+        <Form.Group size="lg" controlid="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            autoFocus
+            type="username"
+            value={username}
+            onChange={(n) => getName(n.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlid="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(n) => getPass(n.target.value)}
+          />
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          size="lg"
+          type="submit"
+          disabled={!checkInput()}
+        >
+          Login
+        </Button>
+        <div className="registerTag">Need an account?</div>
+        <div className="registerButton">
+          <Button
+            variant="primary"
+            size="lg"
+            type="register"
+            onClick={() => {
+              toRegister();
+            }}
+          >
+            Register
+          </Button>
         </div>
-    );
+      </Form>
+    </div>
+  );
 }
 
 export class LoginBuilder {
