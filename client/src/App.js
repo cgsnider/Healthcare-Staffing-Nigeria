5c28350fe61e416c181bf52023246d09a83d1c77
CGSnider <cgsnider@outlook.com>
2022-01-01 17:52:37 -0500

    adds job grid

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 
 import Login from './components/pages/Login';
 import Regisration from './components/pages/Regisration';
+import Prof_JobHub from './components/pages/professionals/Prof_JobHub'
 
 function App() {
   return (
     <div className="App">
-        <Regisration/>
+        <Prof_JobHub/>
     </div>
   );
 }
