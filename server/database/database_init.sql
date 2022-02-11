53341e2510289b2bcd4bd1ce3765b1ba94434325
CGSnider <cgsnider@outlook.com>
2022-02-11 14:11:44 -0500

    initializes db

--- server/database/database_init.sql
+++ server/database/database_init.sql
@@ -1,35 +1,108 @@
-
 drop database if exists cmg_staffing_nigeria;
 create database if not exists cmg_staffing_nigeria;
 use cmg_staffing_nigeria;
 
+create table PERSON (
+	ID int not null,
+    primary key(ID)
+);
+
 create table PROFESSIONAL (
-	IdNumber int not null,
-    Email varchar(254) not null,
-    Verified int not null,
-    Image longblob,
-    FName varchar(55),
-    LName varchar(55),
-    College varChar(75),
-    PhoneNumber varchar(20),
-    MDCN varchar(30),
-    primary key(IdNumber)
+  ID int not null,
+  Email varchar(254) not null,
+  Verified int not null,
+  Image longblob,
+  FName varchar(55),
+  LName varchar(55),
+  College varChar(75),
+  PhoneNumber varchar(20),
+  MDCN varchar(30),
+  primary key(ID),
+  foreign key (ID) references PERSON(ID)
 );
-
 create table COVERLETTER (
-	OwnerId int not null,
-    Title varchar(200),
-    Content text not null,
-    TimeCreated datetime not null,
-    primary key(OwnerId),
-    foreign key (OwnerId) references PROFESSIONAL(IdNumber)
+  OwnerId int not null,
+  Title varchar(200),
+  Content text not null,
+  TimeCreated datetime not null,
+  primary key(OwnerId, TimeCreated),
+  foreign key (OwnerId) references PROFESSIONAL(ID)
+);
+create table DOCUMENTS (
+  OwnerId int not null,
+  FileName varchar(255) not null,
+  Content text not null,
+  TimeCreated datetime not null,
+  primary key(OwnerId, FileName),
+  foreign key (OwnerId) references PROFESSIONAL(ID)
+);
+create table FACILITY (
+  ID int not null,
+  Email varchar(254) not null,
+  Verified int not null,
+  Image longblob,
+  City varchar(50),
+  Country varchar(50),
+  State varchar(50),
+  Street varchar(75),
+  Descript text,
+  FacName varchar(255),
+  primary key(ID),
+  foreign key(ID) references PERSON(ID)
+);
+create table CONTACT (
+  FID int not null,
+  FName varchar(55),
+  LName varchar(55),
+  PhoneNumber varchar(20),
+  primary key(FID),
+  foreign key(FID) references FACILITY(ID)
+);
+create table JOBPOSTING (
+  FID int not null,
+  Title varchar(30) not null,
+  Salary int,
+  Descript text,
+  Slots int not null,
+  primary key (FID, Title),
+  foreign key (FID) references FACILITY(ID)
+);
+create table APPLICAITON (
+  FID int not null,
+  PID int not null,
+  PostingTitle varchar(30),
+  CoverLetter text,
+  TimeCreated datetime not null,
+  primary key (PID, FID),
+  foreign key (FID, PostingTitle) references JOBPOSTING(FID, Title),
+  foreign key (PID) references PROFESSIONAL(ID)
 );
 
-create table DOCUMENTS (
-	OwnerId int not null,
-    FileName varchar(255) not null,
-    Content text not null,
-    TimeCreated datetime not null,
-    primary key(OwnerId),
-    foreign key (OwnerId) references PROFESSIONAL(IdNumber)
+create table MESSAGE (
+  FID int not null,
+  PID int not null,
+  Content text not null,
+  TimeCreated datetime not null,
+  primary key(PID, FID),
+  foreign key (FID) references FACILITY(ID),
+  foreign key (PID) references PROFESSIONAL(ID)
+);
+
+create table SYSTEMLOG (
+	UID int not null,
+    Act varchar(255),
+    TimeCreated datetime,
+    foreign key(UID) references PERSON(ID)
+);
+
+create table ADMINISTRATOR (
+	ID int not null,
+    primary key (ID)
+);
+
+create table PRIVLEGES (
+	AID int not null,
+	privilege varchar(10),
+    primary key (privilege, AID),
+    foreign key (AID) references ADMINISTRATOR(ID)
 );
