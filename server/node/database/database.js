7974c8fc56e2ad509fca918ca0f44154f1e42f98
Nick Scholz <nickscholz@Nicks-MacBook-Pro.local>
2022-02-14 17:00:10 -0500

    database.js root1 giving error changed to just root

--- server/node/database/database.js
+++ server/node/database/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
 
     host: 'localhost',
-    user: 'root1',
+    user: 'root',
     password: '',
     database: 'cmg_staffing_nigeria'
 })
