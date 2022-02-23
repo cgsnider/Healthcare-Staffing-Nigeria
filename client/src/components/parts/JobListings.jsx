34122e4c3032a3e8b770de21866058e1d43226b0
WilliamLeonard <wleonard3@gatech.edu>
2022-02-23 15:29:28 -0500

    Fixed text wrap for Job postings

--- client/src/components/parts/JobListings.jsx
+++ client/src/components/parts/JobListings.jsx
@@ -7,7 +7,7 @@ export default class JobListing extends React.Component {
             <a href={this.props.link}>
                 <div id='JobList'>
                     <img src={this.props.image} alt="Logo" id="jobImage"/>
-                    <div id='title'>{this.props.position}</div>
+                    <div className='truncate' id='title'>{this.props.position}</div>
                     <div id='loc'>{this.props.location}</div>
                     <div className='details' id='time'>{this.props.shifts}</div>
                     <div className='details' id='pay'>Pay: {this.props.salary}</div>
