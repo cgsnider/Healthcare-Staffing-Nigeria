004cf35adc438b7495df2abe4f5cbc614531c378
WilliamLeonard <wleonard3@gatech.edu>
2022-02-28 13:04:38 -0500

    Fixed text wrap for Job postings and Added TODO components for Pending Verification Page

---
+++ client/src/components/pages/Admins/PendingVerifications.jsx
@@ -0,0 +1,11 @@
+import React, {useEffect, useState} from 'react'
+import { Link } from 'react-router-dom'
+
+function PendingVerifications(props) {
+
+    return (
+        <div/>
+    );
+}
+
+export default PendingVerifications
