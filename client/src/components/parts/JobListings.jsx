c73bacd40104523da4334da3d816c71d1ef6445f
WilliamLeonard <wleonard3@gatech.edu>
2022-03-07 17:48:53 -0500

    Fixed undefined field for job postings and made a few other corrections

--- client/src/components/parts/JobListings.jsx
+++ client/src/components/parts/JobListings.jsx
@@ -8,9 +8,23 @@ export default class JobListing extends React.Component {
                 <div id='JobList'>
                     <img src={this.props.image} alt="Logo" id="jobImage"/>
                     <div className='truncate' id='title'>{this.props.position}</div>
-                    <div id='loc'>{this.props.location}</div>
-                    <div className='details' id='time'>{this.props.shifts}</div>
-                    <div className='details' id='pay'>Pay: {this.props.salary}</div>
+
+                    <div id='loc'>
+                        {(this.props.location === "undefined, undefined") ?
+                            ("Account Unverified") :
+                            (this.props.location)}
+                    </div>
+                    <div className='details' id='time'>
+                        {(this.props.shifts === undefined) ?
+                            ("Unverified") :
+                            (this.props.shifts)}
+                    </div>
+                    <div className='details' id='pay'>
+                        {(this.props.shifts === undefined) ?
+                            ("Unverified") :
+                            <div>Pay: &#8358; {this.props.salary} </div>
+                        }
+                    </div>
                 </div>
             </button>
         );
