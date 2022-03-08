78dea89a988b6c7aa24cbe3bd87a9ce5a7abdcad
Nick Scholz <nickscholz@Nicks-MacBook-Pro.local>
2022-03-07 20:55:12 -0500

    mor merge conflict adjustment

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -198,17 +198,6 @@ export default function Profile(props) {
                                     </div>
                                     <div className="grid grid-cols-2">
                                         <div className="px-4 py-2 font-semibold">MDCN #</div>
-<<<<<<< HEAD
-                                        <div className="px-4 py-2">{profileInfo.MDCN || "None"}</div>
-                                    </div>
-                                    <div className="grid grid-cols-2">
-                                        <div className="px-4 py-2 font-semibold">Contact No.</div>
-                                        <div className="px-4 py-2">+234 {profileInfo.PhoneNumber} {/**not sure how to handle country code formatting */}</div>
-                                    </div>
-                                    <div className="grid grid-cols-2">
-                                        <div className="px-4 py-2 font-semibold">Address</div>
-                                        <div className="px-4 py-2">{(profileInfo.Street=='null')?`${profileInfo.Street}\n ${profileInfo.City}, ${profileInfo.Country}`: "None"}</div>
-=======
                                         <div className="px-4 py-2">{profileInfo.MDCN || 'None'}</div>
                                     </div>
                                     <div className="grid grid-cols-2">
@@ -218,7 +207,6 @@ export default function Profile(props) {
                                     <div className="grid grid-cols-2">
                                         <div className="px-4 py-2 font-semibold">Address</div>
                                         <div className="px-4 py-2">{(profileInfo.Street!==null)?`${profileInfo.Street}\n ${profileInfo.City}, ${profileInfo.Country}`: "None"}</div>
->>>>>>> nullprofile
                                     </div>
                                     <div className="grid grid-cols-2">
                                         <div className="px-4 py-2 font-semibold">Email</div>
@@ -228,11 +216,7 @@ export default function Profile(props) {
                                     </div>
                                     <div className="grid grid-cols-2">
                                         <div className="px-4 py-2 font-semibold">License #</div>
-<<<<<<< HEAD
                                         <div className="px-4 py-2">{profileInfo.LicenseNumber || "None"}</div>
-=======
-                                        <div className="px-4 py-2">{profileInfo.LicenseNumber || 'None'}</div>
->>>>>>> nullprofile
                                     </div>
                                 </div>
                             </div>
@@ -576,10 +560,6 @@ function AboutPopup(props) {
         setInfo({...tempInfo, gender: gender.label});
         setOpen({...open, open:false});
     }
-<<<<<<< HEAD
-
-=======
->>>>>>> nullprofile
     return(
         <div>
             <button className="bg-green-500 rounded text-white text-sm py-1 px-3 ml-auto" onClick={e=>setOpen({fresh:true, open:true})}>Edit</button>
@@ -638,11 +618,7 @@ function AboutPopup(props) {
                             <input type='text' className="rounded" value={tempInfo.Specialization} onInput={e=>setTempInfo({...tempInfo, Specialization: e.target.value})}/>
                         </div>
                         <div className='my-4 w-full col-span-2'>
-<<<<<<< HEAD
-                            <textarea className='w-full rounded' type='textarea' placeholder='Short Bio' onInput={e=>setTempInfo({...tempInfo, Bio: e.target.value})} value={tempInfo.Bio}></textarea>
-=======
                             <textarea className='w-full rounded' type='textarea' placeholder='Short Bio' onInput={e=>setTempInfo({...tempInfo, Bio: e.target.value})} value={(tempInfo.Bio!==null)?tempInfo.Bio:''}></textarea>
->>>>>>> nullprofile
                         </div>
                     </div>
                     
