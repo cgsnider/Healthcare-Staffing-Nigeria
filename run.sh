57861b241018b70e1cf5ca3d5895cabb33e08a9f
CGSnider <cgsnider@outlook.com>
2022-04-12 00:12:09 -0400

    run shell now includes delete_procedures.sql

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ while getopts "cdie" OPTION; do
             mysql -uroot -p < $(pwd)/server/database/initializeDB.sql
             mysql -uroot -p < $(pwd)/server/database/input_procedures.sql
             mysql -uroot -p < $(pwd)/server/database/output_procedures.sql
+            mysql -uroot -p < $(pwd)/server/database/delete_procedures.sql
             mysql -uroot -p < $(pwd)/server/database/sample_data.sql
             ;;
         i)
