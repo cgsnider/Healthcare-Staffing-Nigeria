99e3a49ccf40757cae42d1aef43c239d1f10ba7d
Nick Scholz <nickscholz@Nicks-MacBook-Pro.local>
2022-04-23 19:36:24 -0400

    admin message adjustment

--- client/src/components/parts/MessagePopup.jsx
+++ client/src/components/parts/MessagePopup.jsx
@@ -5,6 +5,7 @@ export default function MessagePopup(props) {
         <Popup open={props.open} closeOnDocumentClick onClose={() => props.setOpen(false)}>
             <div >
                 <div className='text-center text-2xl mb-5'>{`you have been ${props.type=='accept'? "Verified!":"Denied verification"}`}</div>
+                <div className='text-center text-lg mb-2'>Response:</div>
                 <div className='text-lg text-center'>{props.message}</div>
             </div>
         </Popup>
