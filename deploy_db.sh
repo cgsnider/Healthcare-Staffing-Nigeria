0343f6f2c4a622459790888255f2e06e4227d592
CGSnider <cgsnider@outlook.com>
2022-04-24 18:01:15 -0400

    bug fix

--- deploy_db.sh
+++ deploy_db.sh
@@ -1,4 +1,6 @@
+read -p 'DB Password (Just hit enter if there is not one): ' password
+
 sudo mysql -uroot -p $password < $(pwd)/server/database/initializeDB.sql
-sudo mysql -uroot -p  $password < $(pwd)/server/database/input_procedures.sql
+sudo mysql -uroot -p $password < $(pwd)/server/database/input_procedures.sql
 sudo mysql -uroot -p $password < $(pwd)/server/database/output_procedures.sql
-sudo mysql -uroot -p < $(pwd)/server/database/delete_procedures.sql
+sudo mysql -uroot -p $password < $(pwd)/server/database/delete_procedures.sql
