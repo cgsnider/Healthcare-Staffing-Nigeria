7bc30d6208bce8921514aaf5cb17b856bc02c597
CGSnider <cgsnider@outlook.com>
2022-04-25 01:02:20 -0400

    removes aws keys

--- client/src/hooks/cognito_pool.js
+++ client/src/hooks/cognito_pool.js
@@ -1,5 +1,5 @@
 const poolData = {
-   UserPoolId: 'us-east-2_q85GCcTxM', //Enter User Pool ID here
-   ClientId: '2t9l195uocaslipd2ejbslcd7a' // Enter Client ID Here
+   UserPoolId: '', //Enter User Pool ID here
+   ClientId: '' // Enter Client ID Here
 }
 export {poolData};
